46c8f875f89810dff233650fa3414e3a9ae862a4
andreeamacsim <mlaviniaandreea@yahoo.com>
2025-03-24 19:21:23 +0200

    SCRUM-46 Added the front end project and implemented login interface

---
+++ SmartGradesFrontEnd/FrontEnd/src/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, RouterModule, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  errorMessage: string = '';
+  successMessage: string = '';
+
+  constructor(private router: Router) {}
+
+  onLogin() { // TODO : connection with back end --- dummy implementation
+    if (this.username && this.password) {
+      console.log('Login attempt:', this.username);
+      this.successMessage = 'Login successful!';
+      this.errorMessage = ''; 
+    } else {
+      this.errorMessage = 'Please enter username and password';
+      this.successMessage = ''; 
+    }
+  }
+}
