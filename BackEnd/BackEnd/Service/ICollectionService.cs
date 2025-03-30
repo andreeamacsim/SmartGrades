dbf70964bb87d6f8081005f269db3b1817c99f91
MihaiMoisescu <mihaimoisescu9@gmail.com>
2025-03-30 21:24:01 +0300

    SCRUM-36 Add ICollectionService interface

---
+++ BackEnd/BackEnd/Service/ICollectionService.cs
@@ -0,0 +1,11 @@
+﻿namespace BackEnd.Service
+{
+    public interface ICollectionService<T>
+    {
+        Task<bool> Create(T enity);
+        Task<bool> Delete(T enity);
+        Task<bool> Update(string id,T enity);
+        Task<T> GetById(string id);
+        Task<List<T>> GetAll();
+    }
+}
