b72383e29b052f0293d8afa94bf57bbbe4562cc9
MihaiMoisescu <mihaimoisescu9@gmail.com>
2025-03-30 20:27:33 +0300

    SCRUM-34 Create models

---
+++ BackEnd/BackEnd/Models/Grade.cs
@@ -0,0 +1,9 @@
+﻿namespace BackEnd.Models
+{
+    public class Grade
+    {
+        public string Id { get; set; }
+        public string CourseId { get; set; }
+        public string AssigmentName { get; set; }
+    }
+}
