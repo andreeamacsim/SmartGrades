f66d0d90d1f6884e6b3f75de14f06507d717940d
rusuuu <136969534+rusuuu@users.noreply.github.com>
2025-04-02 17:04:08 +0300

    SCRUM-16 Implement email sending for password reset

---
+++ BackEnd/BackEnd/Controllers/StudentController.cs
@@ -0,0 +1,97 @@
+﻿using BackEnd.Helpers;
+using BackEnd.Models;
+using BackEnd.Models.Dto;
+using BackEnd.Service;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace BackEnd.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentCollectionService _studentService;
+        private readonly IConfiguration _config;
+        private readonly IEmailService _emailService;
+
+        public StudentController(IStudentCollectionService studentService, IConfiguration configuration, IEmailService emailService)
+        {
+            _studentService = studentService;
+            _config = configuration;
+            _emailService = emailService;
+        }
+
+        [HttpPost("/student/send-reset-email/{email}")]
+        public async Task<IActionResult> SendEmail(string email)
+        {
+            var studentList = await _studentService.GetAll();
+            var student = studentList.FirstOrDefault(s => s.Email == email);
+            if (student == null)
+            {
+                return NotFound(new
+                {
+                    StatusCode = 404,
+                    Message = "Email doesn't exist"
+                });
+            }
+
+            var tokenBytes = RandomNumberGenerator.GetBytes(64);
+            var emailToken = Convert.ToBase64String(tokenBytes);
+            student.ResetPasswordToken = emailToken;
+            student.ResetPasswordExpiry = DateTime.UtcNow.AddMinutes(15);
+
+            await _studentService.Update(student.Id, student);
+
+            string from = _config["EmailSettings:From"];
+            var emailModel = new EmailModel(email, "Reset Password", EmailBody.EmailStringBody(email, emailToken));
+            _emailService.SendEmail(emailModel);
+
+            return Ok(new
+            {
+                StatusCode = 200,
+                Message = "Email sent successfully"
+            });
+        }
+
+        [HttpPost("/student/reset-password")]
+        public async Task<IActionResult> ResetPassword(ResetPasswordDto resetPasswordDto)
+        {
+            var newToken = resetPasswordDto.EmailToken.Replace(" ", "+");
+            var studentList = await _studentService.GetAll();
+            var student = studentList.FirstOrDefault(a => a.Email == resetPasswordDto.Email);
+            if (student == null)
+            {
+                return NotFound(new
+                {
+                    StatusCode = 404,
+                    Message = "Email doesn't exist"
+                });
+            }
+
+            var tokenCode = student.ResetPasswordToken;
+            DateTime emailTokenExpiry = student.ResetPasswordExpiry;
+
+            if (tokenCode != resetPasswordDto.EmailToken || emailTokenExpiry < DateTime.UtcNow)
+            {
+                return BadRequest(new
+                {
+                    StatusCode = 400,
+                    Message = "Invalid reset link"
+                });
+            }
+
+            student.Password = resetPasswordDto.NewPassword;
+
+            await _studentService.Update(student.Id, student);
+
+            return Ok(new
+            {
+                StatusCode = 200,
+                Message = "Password reset successfully"
+            });
+        }
+
+    }
+}
---
+++ BackEnd/BackEnd/Helpers/EmailBody.cs
@@ -0,0 +1,30 @@
+﻿namespace BackEnd.Helpers
+{
+    public static class EmailBody
+    {
+        public static string EmailStringBody(string email, string emailToken)
+        {
+            return $@"
+        <html>
+            <head> </head>
+            <body style=""margin:0;padding:0;font-family: Arial, Helvetica, sans-serif;"">
+                <div style height: auto; background: linear gradient(to top, #c9c9ff 50%, #6eef6 90%) no-repeat width:400px; padding: 30px;>
+                    <div>
+                        <div>
+                            <h1> Reset your Password </h1>
+                            <hr>
+                            <p>You're receiving this email because you requested a password reset for your Smart Grades account.</p>
+                            <p>Click the button below to reset your password. If you didn't request a password reset, you can ignore this email.</p>
+                            <a href=""http://localhost:4200/forgot-password?email={email}&code={emailToken}"" target=""_blank"" style=""background-color: #0d6efd;padding:10px;border:none 
+                            color: white; border-radius:4px;display:block;margin:0 auto;width:50%;text-align:center;padding:10px;text-decoration:none;"">Reset Password</a>
+
+                            <p>Kindest regards,<br><br>Smart Grades Team</p>
+                        </div>
+                    </div>
+                </div>
+            </body>
+        </html>";
+        }
+    }
+
+}
